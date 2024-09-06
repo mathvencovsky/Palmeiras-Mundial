2ce15f4a503a91721a1a29e27612ae523303ca57
Matheus Vencovsky da Penha <98749369+mathvencovsky@users.noreply.github.com>
2024-09-06 17:52:50 +0200

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo : "1951",
+        descricao : "O Palmeiras nunca conquistou o título da competição na sua forma atual, o que reforça a posição de quem não considera a Copa Rio como um título mundial.",
+        link : "https://pt.wikipedia.org/wiki/Caninha_51"
+    },
+    {
+        titulo : "2002",
+        descricao : "Em 2002, um dos momentos mais marcantes e dolorosos da história do Palmeiras aconteceu: o rebaixamento para a Série B do Campeonato Brasileiro.",
+        link : "https://www.youtube.com/watch?v=GHwgspRAf5o"
+    },
+    {
+        titulo : "2012",
+        descricao : "Apenas dez anos após o primeiro rebaixamento para a Série B, o Palmeiras vivenciou novamente a amargura de uma queda para a segunda divisão do Campeonato Brasileiro em 2012.",
+        link : "https://www.youtube.com/watch?v=Oef04DUYPcc"
+    }
+];
+
