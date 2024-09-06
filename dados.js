8ad2d67e2d315f29b60a2197e1730bc2368b7f7a
Matheus Vencovsky da Penha <98749369+mathvencovsky@users.noreply.github.com>
2024-09-06 19:17:33 +0200

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,18 +1,45 @@
 let dados = [
     {
         titulo : "1951",
-        descricao : "O Palmeiras nunca conquistou o título da competição na sua forma atual, o que reforça a posição de quem não considera a Copa Rio como um título mundial.",
-        link : "https://pt.wikipedia.org/wiki/Caninha_51"
+        descricao : "O Palmeiras nunca conquistou o título mundial. 51 é Pinga.",
+        link : "https://pt.wikipedia.org/wiki/Caninha_51",
+        tags: "51 copa_rio copa rio"
     },
     {
         titulo : "2002",
         descricao : "Em 2002, um dos momentos mais marcantes e dolorosos da história do Palmeiras aconteceu: o rebaixamento para a Série B do Campeonato Brasileiro.",
-        link : "https://www.youtube.com/watch?v=GHwgspRAf5o"
+        link : "https://www.youtube.com/watch?v=zf5RhMVfyQQ",
+        tags: "marcos penta rebaixado rebaixamento"
     },
     {
         titulo : "2012",
         descricao : "Apenas dez anos após o primeiro rebaixamento para a Série B, o Palmeiras vivenciou novamente a amargura de uma queda para a segunda divisão do Campeonato Brasileiro em 2012.",
-        link : "https://www.youtube.com/watch?v=Oef04DUYPcc"
+        link : "https://www.youtube.com/watch?v=zf5RhMVfyQQ",
+        tags: "serie_b rebaixamento rebaixado"
+    },
+    {
+        titulo : "1999",
+        descricao : "A partida foi decidida por um gol do capitão do Manchester United, Roy Keane, em um lance polêmico no primeiro tempo. O gol do irlandês, após uma falha do goleiro Marcos, foi suficiente para dar a vitória aos ingleses.",
+        link : "https://www.youtube.com/watch?v=fcT5Qj8Vw8Q",
+        tags: "interclubes falha manchester manchester_united"
+    },
+    {
+        titulo : "2021",
+        descricao : "O time inglês, comandado por Thomas Tuchel, conquistou o título após vencer o Palmeiras na final.",
+        link : "https://www.youtube.com/watch?v=mJpOWLeHAVg",
+        tags: "mundial_2021 2021 Havertz chelsea havertz lukaku"
+    },
+    {
+        titulo : "2020",
+        descricao : "A eliminação nas semifinais gerou uma grande frustração na torcida palmeirense, que via no Mundial de Clubes a oportunidade de conquistar um título inédito.",
+        link : "https://www.youtube.com/watch?v=rO11LmYMQ-o",
+        tags: "mundial_2020 2020 tigres mexico gignac"
+    },
+    {
+        titulo : "Não Ganhou",
+        descricao : "A FIFA, até o momento, não reconheceu oficialmente a Copa Rio como um Mundial de Clubes. A entidade máxima do futebol mundial tem seus próprios critérios para definir o que é um Mundial de Clubes e, por enquanto, a Copa Rio não se encaixa nesses critérios.",
+        link : "https://www.youtube.com/watch?v=zf5RhMVfyQQ",
+        tags: "fifa campeao campeão abel  libertadores verde verdão verdao porco 1914 1915 1916 1917 1918 1919 1920 1921 1922 1923 1924 1925 1926 1927 1928 1929 1930 1931 1932 1933 1934 1935 1936 1937 1938 1939 1940 1941 1942 1943 1944 1945 1946 1947 1948 1949 1950 1952 1953 1954 1955 1956 1957 1958 1959 1960 1961 1962 1963 1964 1965 1966 1967 1968 1969 1970 1971 1972 1973 1974 1975 1976 1977 1978 1979 1980 1981 1982 1983 1984 1985 1986 1987 1988 1989 1990 1991 1992 1993 1994 1995 1996 1997 1998 2000 2001 2003 2004 2005 2006 2007 2008 2009 2010 2011 2013 2014 2015 2016 2017 2018 2019 2022 2023 2024 2025 ganhou ganhou_sim gamba gambá corinthians"
     }
 ];
 
