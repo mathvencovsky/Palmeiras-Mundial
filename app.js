0542a39b6afb27e850f0c0320a88122ab37d4175
Matheus Vencovsky da Penha <98749369+mathvencovsky@users.noreply.github.com>
2024-09-06 20:22:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (campoPesquisa == " ") {
+        section.innerHTML = "<p>O PALMEIRAS AINDA NÃO GANHOU O MUNDIAL.</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    console.log(campoPesquisa);
   
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // Se titulos includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
       // Cria um novo elemento div para cada resultado
       resultados += `
         <div class="item-resultado">
@@ -17,8 +36,14 @@ function pesquisar() {
           <a href=${dado.link} target="_blank">Descubra a verdade</a>
         </div>
       `;
+        }
     }
-  
+
+    if (!resultados) {
+        // Personaliza a mensagem com a palavra pesquisada
+        resultados = `<p>Você pesquisou por "${campoPesquisa}". Será que o Palmeiras vai ganhar o Mundial procurando por isso? Tente outra palavra-chave amanhã!</p>`;
+      }
+
     // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
   }
